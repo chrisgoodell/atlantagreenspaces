723057852646a4d2f53e619c4beace2467643e33
David Byas-Smith <dbyassmith@neboagency.com>
2016-04-25 13:02:38 -0400

    removed google maps code and added calculation of distance locally

--- src/app/js/controllers.js
+++ src/app/js/controllers.js
@@ -28,10 +28,27 @@ greenspaceControllers.controller('GreenspaceListCtrl', ['$scope', '$http','$stat
       function success(position) {
 
         var coords = position.coords;
-        var currentLocation = new google.maps.LatLng(coords.latitude,coords.longitude);
+
+        function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+          var R = 6371; // Radius of the earth in km
+          var dLat = deg2rad(lat2-lat1);  // deg2rad below
+          var dLon = deg2rad(lon2-lon1); 
+          var a = 
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+            ; 
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+          var d = R * c * 0.621371; // Distance in miles
+          return d;
+        }
+
+        function deg2rad(deg) {
+          return deg * (Math.PI/180)
+        }
+
         for(var i in data){
-          var loc = new google.maps.LatLng(data[i].lat,data[i].long);
-          var distance = google.maps.geometry.spherical.computeDistanceBetween(loc, currentLocation) * 0.000621371192;
+          var distance = getDistanceFromLatLonInKm(coords.latitude,coords.longitude,data[i].lat,data[i].long);
           data[i].distance = (Math.round(distance * 100) / 100);
         }
         
